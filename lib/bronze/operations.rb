251ee0fff5916ad9f99e442142d26e8de25835c9
sleepingkingstudios <merlin@sleepingkingstudios.com>
2016-09-07 17:34:45 -0400

    Implement Bronze::Operations::Operation.

---
+++ lib/bronze/operations.rb
@@ -0,0 +1,9 @@
+# lib/bronze/operations.rb
+
+require 'bronze'
+
+module Bronze
+  # Namespace for operation classes, which encapsulate a process or procedure of
+  # business logic with a consistent architecture and interface.
+  module Operations; end
+end # module
