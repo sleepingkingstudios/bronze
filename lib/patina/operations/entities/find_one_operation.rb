67f3566742ac222414e7ca7d2c70e6bdce07bb7b
sleepingkingstudios <merlin@sleepingkingstudios.com>
2017-02-13 14:12:29 -0500

    Extract Patina::Operations::Entities::PersistenceOperation.

--- lib/patina/operations/entities/find_one_operation.rb
+++ lib/patina/operations/entities/find_one_operation.rb
@@ -1,45 +1,21 @@
 # lib/patina/operations/entities/find_one_operation.rb
 
-require 'sleeping_king_studios/tools/toolbelt'
-
 require 'bronze/operations/operation'
 
 require 'patina/operations/entities'
 require 'patina/operations/entities/error_messages'
-require 'patina/operations/entities/transforms/persistence_transform'
+require 'patina/operations/entities/persistence_operation'
 
 module Patina::Operations::Entities
   # Queries the repository for the record with the given class and primary key.
   class FindOneOperation < Bronze::Operations::Operation
     include Patina::Operations::Entities::ErrorMessages
-
-    # @param repository [Bronze::Collections::Repository] The repository used to
-    #   query the resource and any child resources.
-    # @param resource_class [Class] The class of entity to query for.
-    def initialize repository, resource_class
-      @repository     = repository
-      @resource_class = resource_class
-    end # constructor
-
-    # @return [Bronze::Collections::Repository] The repository used to query the
-    #   resource and any child resources.
-    attr_reader :repository
+    include Patina::Operations::Entities::PersistenceOperation
 
     # @return [Bronze::Entities::Entity] The found resource, if any.
     attr_reader :resource
 
-    # @return [Class] The class of entity to query for.
-    attr_reader :resource_class
-
     private
-
-    def collection
-      repository.collection(plural_resource_name.intern, transform)
-    end # method collection
-
-    def plural_resource_name
-      @plural_resource_name ||= tools.string.pluralize(resource_name)
-    end # method plural_resource_name
 
     def process primary_key
       raise ArgumentError, "can't be nil" if primary_key.nil?
@@ -57,29 +33,5 @@ module Patina::Operations::Entities
         :id => primary_key
       ) # end errors
     end # method process
-
-    def resource_name
-      @resource_name ||=
-        begin
-          name = resource_class.name.split('::').last
-          name = tools.string.underscore(name)
-
-          tools.string.singularize(name)
-        end # resource_name
-    end # method resource_name
-
-    def tools
-      SleepingKingStudios::Tools::Toolbelt.instance
-    end # method tools
-
-    def transform
-      @transform ||=
-        begin
-          transform_class =
-            Patina::Operations::Entities::Transforms::PersistenceTransform
-
-          transform_class.new(resource_class)
-        end # begin
-    end # method transform
   end # class
 end # module
---
+++ lib/patina/operations/entities/persistence_operation.rb
@@ -0,0 +1,62 @@
+# lib/patina/operations/entities/persistence_operation.rb
+
+require 'sleeping_king_studios/tools/toolbelt'
+
+require 'patina/operations/entities'
+require 'patina/operations/entities/transforms/persistence_transform'
+
+module Patina::Operations::Entities
+  # Shared functionality for operations that query a resource from a repository
+  # or persist the resource to the repository.
+  module PersistenceOperation
+    # @param repository [Bronze::Collections::Repository] The repository used to
+    #   query the resource and any child resources.
+    # @param resource_class [Class] The class of entity to query for.
+    def initialize repository, resource_class, transform = nil
+      @repository     = repository
+      @resource_class = resource_class
+      @transform      = transform
+    end # constructor
+
+    # @return [Bronze::Collections::Repository] The repository used to query the
+    #   resource and any child resources.
+    attr_reader :repository
+
+    # @return [Class] The class of entity to query for.
+    attr_reader :resource_class
+
+    private
+
+    def collection
+      @collection ||= repository.collection(plural_resource_name, transform)
+    end # method collection
+
+    def plural_resource_name
+      @plural_resource_name ||= tools.string.pluralize(resource_name)
+    end # method plural_resource_name
+
+    def resource_name
+      @resource_name ||=
+        begin
+          name = resource_class.name.split('::').last
+          name = tools.string.underscore(name)
+
+          tools.string.singularize(name)
+        end # resource_name
+    end # method resource_name
+
+    def tools
+      SleepingKingStudios::Tools::Toolbelt.instance
+    end # method tools
+
+    def transform
+      @transform ||=
+        begin
+          transform_class =
+            Patina::Operations::Entities::Transforms::PersistenceTransform
+
+          transform_class.new(resource_class)
+        end # begin
+    end # method transform
+  end # module
+end # module
