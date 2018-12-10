30c6f3ffa66a87b2c62725c9fbae4be6d572dc05
Rob Smith <merlin@sleepingkingstudios.com>
2018-12-09 23:03:37 -0500

    Implement Entities::Attribute::Builder.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,7 @@ require 'byebug'
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.extend  RSpec::SleepingKingStudios::Concerns::ExampleConstants
   config.extend  RSpec::SleepingKingStudios::Concerns::FocusExamples
   config.extend  RSpec::SleepingKingStudios::Concerns::WrapExamples
   config.include RSpec::SleepingKingStudios::Examples::PropertyExamples
