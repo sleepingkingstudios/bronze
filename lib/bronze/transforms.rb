7c2668995747766323e35d4eeee3a8785607f885
Rob Smith <merlin@sleepingkingstudios.com>
2018-12-16 21:58:14 -0500

    Implement Transforms::Transform.

---
+++ lib/bronze/transforms.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'bronze'
+
+module Bronze
+  # Namespace for defining transform objects, which that map a data object into
+  # another representation of that data.
+  module Transforms; end
+end
